317f78f0bd3fe15896cfb8bbb3c09a535595d832
Claudia4lvarez <claudiaalvarezvitola@gmail.com>
2021-11-14 12:12:13 -0500

    se pone a punto authapp

--- authApp/serializers/userSerializer.py
+++ authApp/serializers/userSerializer.py
@@ -5,5 +5,5 @@ from authApp.models.user import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'name', 'email', 'account']
+        fields = ['id', 'username', 'password', 'name', 'email']
 
